1ad30421f7b0e1997d732d16946c5596262de869
poojasaxena2000 <poojags2805@gmail.com>
2024-09-05 19:30:23 +0530

    final landingpage

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import  Navbar from "./components/Navbar";
+import  HeroSection from "./components/HeroSection";
+import  Feature from "./components/Feature";
+import Workflow from "./components/Workflow";
+import  Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+     < Navbar/>
+     <div className='max-w-7xl mx-auto pt-20 px-6'>
+     < HeroSection/>
+     < Feature/>
+     <Workflow/>
+     < Pricing/>
+     <Testimonials/>
+     <Footer/>
+
+     </div>
+    
+
+    </div>
+  )
+}
+
+export default App
